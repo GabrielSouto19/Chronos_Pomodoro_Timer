c1f92eff36cb4246c5cc8ffd61d6365e1d5e191a
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-12-18 13:32:37 -0300

    css components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,10 @@ import { Heading } from './components/Heading'
 export function App(){
   
   return <>
-    <Heading/>
+    <Heading attr={123}>Olá Mundo!</Heading>
+
+
+
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat assumenda sapiente tempora esse ratione totam eligendi delectus officiis ad dolores, a laudantium tempore itaque consequatur! Voluptatum debitis minus laudantium! Expedita.</p>
   </>
 
--- app/src/components/Heading.tsx
+++ app/src/components/Heading.tsx
@@ -1,7 +1,7 @@
 import styles from './Heading.module.css'
 
 
-export function Heading(){
-    console.log(styles)
-    return <h1 className={styles.heading}>Olá Mundo</h1>
+export function Heading(props){
+    console.log(props)
+    return <h1 className={styles.heading}>{props.children} e {props.attr}</h1>
 }
