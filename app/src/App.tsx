14ddecac3f38dee16505fb72d902ab3963a5c9d1
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-12-18 12:44:15 -0300

    insrindo styles components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,14 @@
 // React utiliza PascalCase para nome de funções
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading'
+
 
 export function App(){
   
-  return <h1>Olá Mundo!</h1>
+  return <>
+    <Heading/>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat assumenda sapiente tempora esse ratione totam eligendi delectus officiis ad dolores, a laudantium tempore itaque consequatur! Voluptatum debitis minus laudantium! Expedita.</p>
+  </>
 
 };
