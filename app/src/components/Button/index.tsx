6bb0227136a4d1ead585c4f038dadb699f99c73d
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-12-27 21:43:43 -0300

    aumentando o tamanho da margin do component Button

--- app/src/components/Button/index.tsx
+++ app/src/components/Button/index.tsx
@@ -3,13 +3,14 @@ import styles from './styles.module.css'
 
 type ButtonProps = {
     icon: React.ReactNode;
+    color?:"green" | "red";
 
 // interrogação representa que  o preencimetno é ocional
 } & React.ComponentProps<'button'>
 
 
 
-export function Button({icon, ...props}:ButtonProps){
+export function Button({icon,color="green", ...props}:ButtonProps){
     return <>
         <button className={styles.button}  {...props} >
             {icon}
