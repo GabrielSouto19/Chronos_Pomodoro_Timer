a50e54a7551f0387729720928c07d2c1cb5d9567
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2026-01-01 20:19:04 -0300

    implementando o state com lótgica em js puro,

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,24 +1,52 @@
 import {HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react'
 import styles from './styles.module.css'
+import { useState } from 'react'
 
+
+
+type AvailableThemes = 'dark'|'light'
 
 
 export function Menu(){
-    
+
+    const [theme,setTheme] = useState<AvailableThemes>("dark")
+
+    function handleThemeChange(
+        event:React.MouseEvent<HTMLAnchorElement>
+    ){
+        event.preventDefault()
+
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === "dark"? "light": "dark"
+            return nextTheme
+        })
+
+        if (theme == "dark"){
+            setTheme("light")
+        }else{
+            setTheme("dark")
+
+        }
+
+        console.log(theme)
+        
+    }
+
     return <nav className={styles.menu}>
-        <a href="#" className={styles.menuLink}>
+        <h1>{theme}</h1>
+        <a href="#" className={styles.menuLink} aria-label='Ir para Home' title='Home'>
             <HomeIcon size={40}/>  
         </a>
         
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='ir para Histórico' title='Ver Histórico'>
             <HistoryIcon size={40}/>  
         </a>
         
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='Ir para configurações' title='Configurações'>
             <SettingsIcon size={40}/>  
         </a>
         
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label='Mudar Tema' title='Mudar Tema' onClick={handleThemeChange} >
             <SunIcon size={40}/>  
         </a>
     </nav>
